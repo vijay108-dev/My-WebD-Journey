90fe892a9287bdbb875aa38efa1f98ef4f01f149
vijay108-dev <vijay108.dev@gmail.com>
2026-01-01 11:09:19 +0530

    Add: Day-24: Animation

---
+++ Day-25/one.js
@@ -0,0 +1,15 @@
+// console.log("Hello");
+
+// let const var
+// var is old
+
+let a =10;
+console.log(a);
+
+let b="Vijay";
+b="Sanya";
+console.log(b);
+
+const c=23;
+console.log(c);
+// c=35; value can't change
