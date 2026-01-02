2b434b27e084d015f6613579e0e165c478e13780
vijay108-dev <vijay108.dev@gmail.com>
2026-01-02 10:28:55 +0530

    Add: Day-26: Datatypes in JavaScript

---
+++ Day-26/one.js
@@ -0,0 +1,66 @@
+//Declare the variable
+// let a;
+
+//intialize the value
+// a=20;
+// console.log(a);
+
+//cd My-WebD-Journey
+//cd Day-26
+//node one.js for run ang you will get your answer
+
+
+// JavaScript has 7 primitive data types:
+// Number – numbers
+// String – text
+// Boolean – true or false
+// Undefined – no value given
+// Null – empty value
+// Symbol – unique value
+// BigInt – very big numbers
+
+//Non-primitive Datatypes
+//Array, Object, function
+
+//Number
+// let age = 25;
+// console.log(age);
+
+//String
+// let name = "Vijay";
+// console.log(name);
+// console.log(typeof name);
+
+//you also write string this way 
+// let str = 'Hello my friends';
+// console.log(str);
+
+//Boolean
+// let isStudent = true;
+//console.log(isStudent);
+
+//Undefined
+// let account;
+// console.log(account); // undefined
+
+//Null
+// let bal = null;
+//console.log(bal);
+//console.log(typeof bal); //object
+
+// I have intentionally not put anything in this variable
+//this is js error that is why he show typeof is object
+//New versions keep coming but we cannot change it because old legacy code
+//that is used in companies is still in use today
+
+
+//Symbol
+// let id = Symbol("id");
+
+//BigInt
+// let bigNumber = 12345678901234567890n;
+// console.log(bigNumber);
+
+
+// console.log(Number.MAX_SAFE_INTEGER);
+// console.log(Number.MIN_SAFE_INTEGER);
