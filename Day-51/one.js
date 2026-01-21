8c82300cc138af276217260fdb805b12752a23c1
vijay108-dev <vijay108.dev@gmail.com>
2026-01-21 10:08:55 +0530

    Add: Day-51: Async Task working and Web API in JavaScript

---
+++ Day-51/one.js
@@ -0,0 +1,71 @@
+// Web API stands for Web Application Programming Interface.
+// A Web API is a way for one application to talk to another application over the internet using HTTP.
+// Web API = Bridge between client and server
+
+// Client → Browser / Mobile App / Frontend
+// Server → Backend / Database
+// Web API → Rules + URLs that connect both
+
+// Example
+// Think of Zomato:
+// You open the app (client)
+// App asks: “Give me restaurants near me”
+// Zomato server responds with data
+// This request–response happens via a Web API.
+
+// console.log("Hello");
+
+// function meet(){
+//     const arr = [2,4,6];
+//     console.log(arr[0]);
+// }
+
+// function greet(){
+//     const a = 2 + 3;
+//     console.log(a);
+//     meet();
+//     console.log(a*a);
+// }
+
+// greet();
+// console.log("Program End");
+
+
+// console.log("HEllo");
+
+// setTimeout(()=>{
+//   const a = 2+4;
+//   console.log(a);
+// },5000);
+
+// let b = 20;
+// let arr = [20,30,11];
+
+// for(let i of arr)
+//     console.log(i*b);
+
+
+// console.log("HEllo");
+
+// setTimeout(()=>{
+//   const a = 2+4;
+//   console.log(a);
+// },5000);
+
+// setInterval(() => {
+//     console.log("I am fast");
+// }, 2000);
+
+// let b = 20;
+// let arr = [20,30,11];
+
+// for(let i of arr)
+//     console.log(i*b);
+
+
+// console.log("Hello");
+
+// fetch("https://youtube.com")
+// .then(()=>console.log("Hello"));
+
+// console.log("I am last");
