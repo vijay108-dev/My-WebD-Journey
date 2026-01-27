8462e2ca5bce17803469f8dfca8f59d783598ebe
vijay108-dev <vijay108.dev@gmail.com>
2026-01-27 16:20:10 +0530

    Add: Day-56: TypeScript

---
+++ Day-56/one.js
@@ -0,0 +1,5 @@
+"use strict";
+let a = 10;
+let b = 20;
+const c = "vijay";
+let str = "vijay";
---
+++ Day-56/one.ts
@@ -0,0 +1,6 @@
+let a:number = 10;
+let b:number = 20;
+
+const c:string = "vijay";
+
+let str:string="vijay";
