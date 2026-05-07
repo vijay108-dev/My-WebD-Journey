4c1ce8502234dfc74a077b9cd36d12625f044311
vijay108-dev <vijay108.dev@gmail.com>
2026-05-07 09:42:23 +0530

    Add: Day-73: Virtual DOM in React

---
+++ Day-73/one.js
@@ -0,0 +1,5 @@
+//react will make batch but js not we write extra code for batch it is like fragment
+//React will execute code easily but not js bcz it is synchronous
+// react will do automaytically batch update
+
+//when your code will run one time then it will make a virtual DOM it is a lightweight
